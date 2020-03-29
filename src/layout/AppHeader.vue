a6bbaf1196486777ca36ca0eaff82fc8944f849d
Azerus <kevinm@mybestplace.fr>
2020-03-29 17:10:45 +0200

    fix

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,20 +1,9 @@
 <template>
     <header class="header-global">
-        <base-nav class="navbar-main" transparent type="" effect="light" expand>
+        <base-nav class="navbar-main" transparent type="" effect="dark" expand>
             <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
                 <!--<img src="img/brand/white.png" alt="logo">-->
             </router-link>
-
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
-                    </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
-            </div>
 
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
